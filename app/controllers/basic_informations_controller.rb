00aa6fb277c7915721855c5b5baff2bfbf25aa30
Ian <a9421015642@gmail.com>
2016-03-05 22:29:21 +0800

    add basicInformation controller & view

--- app/controllers/basic_informations_controller.rb
+++ app/controllers/basic_informations_controller.rb
@@ -3,4 +3,21 @@ class BasicInformationsController < ApplicationController
   def index
   end
 
+  def new
+    @basic_information = BasicInformation.new
+  end
+  def create
+    @basic_information = BasicInformation.new(basic_information_params) 
+    if @basic_information.save!
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end
+
+  private
+  def basic_information_params    
+    params.require(:basic_information).permit(:chinesename, :englishname, :gender, :birthday ,:military_service ,:phone ,:telephone ,:address ,:educational_background ,:other_educational_background,:user_id)
+  end
+
 end
